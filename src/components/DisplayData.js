cdad53cd6ec8e79322cacbf5004289d96b6300d6
K3be <ahmed.13.alkaaby@gmail.com>
2020-01-08 17:24:10 +0000

    Done.

---
+++ src/components/DisplayData.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default class DisplayData extends React.Component {
+    render() {
+        return (
+            <div>
+                <h2>{this.props.formData.firstName}</h2>
+                <h2>{this.props.formData.lastName}</h2>
+            </div>
+        )
+    }
+}
